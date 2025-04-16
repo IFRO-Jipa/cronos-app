bab6c371fb77ed0c9a9f704d1b1c59a7481dbf30
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2025-04-16 09:11:23 -0400

    Update deploy.sh

--- .github/devops/cd/deploy.sh
+++ .github/devops/cd/deploy.sh
@@ -2,4 +2,16 @@
 
 set -xe;
 
-echo "olá, mundo!";
+HELM_REPO=https://stakater.github.io/stakater-charts
+HELM_CHART=application
+HELM_CHART_VERSION=6.0.2
+
+DEPLOY_NAME=cronos-app-front-end
+DEPLOY_NAMESPACE=cronos-app
+
+helm upgrade --timeout 10m0s -i ${DEPLOY_NAME} --create-namespace -n ${DEPLOY_NAMESPACE} --repo ${HELM_REPO} ${HELM_CHART} --version ${HELM_CHART_VERSION} --values=./values.yml;
+
+kubectl rollout restart \
+  deployment.apps/${K8S_DEPLOYMENT} \
+  --namespace ${K8S_NAMESPACE} \
+;
